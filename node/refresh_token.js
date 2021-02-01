ed43fc4468357fd58ed91fbf4a1e5d7777ca40eb
Nikzprojects <nikzprojects@gmail.com>
2021-02-01 16:03:55 -0500

    wrote js script to refresh access token automatically

---
+++ node/refresh_token.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const axios = require("axios");
+
+const access_keys = require("../static/data/access_keys.json")
+const old_token = access_keys.access_key
+
+async function getDatafromIG(access_key) {
+  const url = "https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=" + access_key;
+  const getData = async url => {
+    try {
+      const response = await axios.get(url)
+      const data = response.data
+      return await data
+    } catch(error) {
+      console.log(error)
+    }
+  };
+  return await getData(url);
+}
+
+getDatafromIG(old_token).then(data => {
+  console.log(data)
+  const new_token = data.access_token
+  const path = "../static/data/access_keys.json"
+  access_keys["access_key"] = new_token
+
+  console.log(new_token)
+
+  fs.writeFile(path, JSON.stringify(access_keys), (err => {
+    if (err){
+      throw err
+    } else {
+    console.log("Sucess! Instagram access token is now up-to-date.");
+    };
+  }))
+})
