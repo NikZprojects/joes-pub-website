ae04b09cc95d5ccd02b20837b9a00dddc3814923
NikZprojects <NikZprojects@gmail.com>
2022-01-13 22:27:24 -0500

    Main framework and homepage complete

---
+++ next-app/pages/Instagram.js
@@ -0,0 +1,14 @@
+export default function Instagram() {
+  return (
+    <div
+      style={{
+        minHeight: "80vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      This page is coming soon!
+    </div>
+  );
+}
--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -1,13 +1,17 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import logo from "../public/JPsLogo.svg";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Welcome to</h1>
+        <div className={styles.logoContainer}>
+          <Image className={styles.logo} src={logo} alt="Splash Logo" />
+        </div>
+        <div className={styles.locationsContainer}></div>
       </main>
     </div>
   );
