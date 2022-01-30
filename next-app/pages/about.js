354ce22167864e6c584999bb6de46efb5dce9d98
NikZprojects <NikZprojects@gmail.com>
2022-01-30 11:33:55 -0500

    About page edits

--- next-app/pages/about.js
+++ next-app/pages/about.js
@@ -32,58 +32,6 @@ export default function About() {
           great in their own right, but a community to enjoy together built on
           mutual respect and interest can&apos;t be beaten.
         </p>
-        <h2>Rules</h2>
-        <ul>
-          <li>
-            Joe&apos;s Pub is a place where people can gather and have fun, if
-            anything threatens that, it will be removed.
-          </li>
-          <li>
-            We work on the honor system. We expect that all of our patrons are
-            honest about their legal age.
-          </li>
-          <li>
-            There is no last call, but everybody must be gone or going by 12:20
-            AM (local time)
-          </li>
-          <li>
-            We do not condone inebriated driving. Accommodations may be provided
-            at our sister establishment—Joe&apos;s Lounge—for anyone who seems
-            incapable of operating a motor vehicle.
-          </li>
-          <li> The bartender&apos;s decision is always final</li>
-        </ul>
-        <h2> About Our Prices</h2>
-        <p>
-          Everything is for sale.... for the right price. We charge enough to
-          breakeven. Not looking to turn a profit, not looking to rip anyone
-          off, we just want some compensation for what we use.
-        </p>
-        <p>
-          Discounts, coupons, and special tastings are available, but granted at
-          the bartender&apos;s discretion. Chores, tasks, and odd jobs can be
-          used for coupons or credits.
-        </p>
-        <p>
-          No compensation is required if your legal surname is, or may become,
-          “Izzo”.
-        </p>
-        <p>
-          {" "}
-          Payment may be made in the form of cash, via Venmo to the username
-          @emubob59, or PayPal to jaizzo92@gmail.com.
-        </p>
-        <p>
-          {" "}
-          If you don’t have means to pay readily available, a tab can be started
-          and paid off at a later date, but this tab cannot exceed 10 USD or 65
-          DKK.
-        </p>
-        <p>
-          Money can be pre-paid and recorded by the bartender, but refunds are
-          not available.
-        </p>
-        <p>First visit is free.</p>
         <h2> Personnel</h2>
         <p>Joseph A. Izzo – Proprietor, master mixologist, creative genius</p>
         <p>
@@ -91,6 +39,12 @@ export default function About() {
         </p>
         <p>Nikolas J. Zagarella – Webmaster</p>
         <p>Saptarshi Ghosh – Resident bum</p>
+        <h2>Donations</h2>
+        <p>
+          {" "}
+          Donations to Joe's Pub may be made via Venmo to the username
+          @emubob59, or PayPal to jaizzo92@gmail.com.
+        </p>
       </div>
     </div>
   );
