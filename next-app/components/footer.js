710eb2ab560f9449332746410575f05206dda070
Nik Z <nikzprojects@gmail.com>
2022-03-01 19:22:39 -0500

    Remove LLC

--- next-app/components/footer.js
+++ next-app/components/footer.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      &copy;{new Date().getFullYear()} Joe&apos;s Pub, LLC.
+      &copy;{new Date().getFullYear()} Joe&apos;s Pub
     </footer>
   );
 }
